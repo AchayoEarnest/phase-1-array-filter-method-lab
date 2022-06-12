6c4f9cbbb8b39cf5a17e7d0b5ea1bbc4ed4d288f
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-12 07:39:58 +0300

    commit tree d7f663f6690d6a45cab273d679281967c1e2d237

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(driverName, nameString){
+    return driverName.filter(function(string){
+        return((string.toLowerCase()) === nameString.toLowerCase())
+    })
+}
+
+function fuzzyMatch(driverName, letters){
+    return driverName.filter(function(pLetter){
+        if((pLetter.indexOf(letters)) === 0){
+            return true;
+        }else if (pLetter.indexOf(letters)>0){
+            return false;
+        }
+    })
+}
+
+function matchName(driverArr, string){
+    return driverArr.filter(function(element){
+        return element.name === string;
+    })
+}
+
+
+
